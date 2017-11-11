2aaa56dfef09b6fce6ac9307a01e779ac1507443
Kaleb R. Horvath <bobafett2021@hotmail.com>
2017-11-10 15:19:51 -1000

    Create subsort.py

---
+++ subsort.py
@@ -0,0 +1,16 @@
+
+"""
+s = [-5, -23, 5, 0, 23, -6, 23, 67]
+nl = []
+for i in range(len(s)):
+    a = min(s)
+    nl.append(a)
+    s.remove(a)
+
+print nl
+"""
+
+
+# wrapper for the above code
+
+def subsort(list):
