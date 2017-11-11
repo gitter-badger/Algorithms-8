ab50ea2a0979fff391bdd3e6b62da6900a5e26d3
Kaleb R. Horvath <bobafett2021@hotmail.com>
2017-11-11 07:26:16 -1000

    Update subsort.py

--- subsort.py
+++ subsort.py
@@ -1,23 +1,60 @@
 
 """
-s = [-5, -23, 5, 0, 23, -6, 23, 67]
-nl = []
-for i in range(len(s)):
-    a = min(s)
-    nl.append(a)
-    s.remove(a)
+Kaleb R. Horvath
+November 11th, 2017 (START)
+November 11th, 2017 (END)
+7:09 AM (START)
+7:__ AM (END)
+---------------------
+A sorting algorithm
+written in Python 3.
 
-print nl
 """
 
+# Algorithm:
 
-# wrapper for the above code
+"""
+[3, 6, 0, -2, -9]
 
-def subsort(list):
-    nl = []
-    for i in range(len(list)):
-        a = min(s)
-        nl.append(a)
-        s.remove(a)
-    return nl
+append -9, remove -9
+append -2, remove -2
+...
+
+[-9, -2, 0, 3, 6]
+--------------------
+Summary
+--------------------
+Append smallest
+value to new array
+and then remove that
+value from orginal
+and continue on with
+iteration.
+"""
+
+# import basic libraries
+import sys
+import itertools
+
+# create a sort function
+
+def sort(listx):
+	length = len(listx)
+	nl = []
+	for x in range(length):
+		value = min(listx)
+		nl.append(value)
+		listx.remove(value)
+	return nl
+
+
+
+
+
+
+
+
+
+
+
 
