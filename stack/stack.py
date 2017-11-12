a150c8a18e51ab65093a5ab0faecfb2fbf250ee2
Kaleb R. Horvath <bobafett2021@hotmail.com>
2017-11-11 14:37:45 -1000

    Update stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -1,1 +1,27 @@
+import itertools
 
+"""
+Written by Kaleb R. Horvath
+Create a stack in python3.
+"""
+
+
+"""
+data = (0, 1, 2)
+"""
+
+class Stack(object):
+    def __init__(self, data):
+        self.stack = tuple(data)
+    def push(self, value):
+        stack = list(self.stack)
+        stack.append(value)
+        self.stack = tuple(stack)
+        return self.stack
+    def pop(self):
+        s = list(self.stack)
+        del s[-1]
+        self.stack = tuple(s)
+        return self.stack
+    def size(self):
+        return str(len(self.stack)) + ' b'
