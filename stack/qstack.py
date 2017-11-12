ebf9710c5412fb1ee7cb8cf360cf00862cf29bcb
Kaleb R. Horvath <bobafett2021@hotmail.com>
2017-11-11 14:43:35 -1000

    Update qstack.py

--- stack/qstack.py
+++ stack/qstack.py
@@ -1,1 +1,26 @@
 
+class QStack(object):
+    def __init__(self, data):
+        self.stack = tuple(data)
+    def push(self, value):
+        stack = list(self.stack)
+        stack.append(value)
+        self.stack = tuple(stack)
+        return self.stack
+    def pop(self, i):
+        s = list(self.stack)
+        del s[-1]
+        self.stack = tuple(s)
+        return self.stack, i
+    def size(self):
+        return str(len(self.stack)) + ' b'
+    def query(self, pos, query):
+        s = list(self.stack)
+        data = s[pos]
+        results = []
+        for X in data:
+          if X==query:
+            results.append(X)
+        return pos, data, results
+      
+        
